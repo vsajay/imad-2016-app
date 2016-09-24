6cfa57a676c94805961601493d9a9296f7f816a3
vsajay <ch16b061@smail.iitm.ac.in>
2016-09-24 14:17:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send("Article1")
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("Article2");
+});
+
+app.get('/article-three', function (req, res) {
+  res.send("Article3");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
